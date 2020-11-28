d2c7e005e02dbf44f516af562374d2c198865cce
Caeglathatur <49456231+Caeglathatur@users.noreply.github.com>
2020-11-28 22:35:46 +0100

    Add --explain

--- stvcount/__main__.py
+++ stvcount/__main__.py
@@ -5,6 +5,15 @@ import typing
 from copy import deepcopy
 
 
+EXPLAIN = False
+
+
+def explain(string):
+    global EXPLAIN
+    if EXPLAIN:
+        print(string)
+
+
 class Candidate:
     def __init__(self, id):
         self.id = id
@@ -43,7 +52,7 @@ def condorcet_pair(
         candidate.num_votes = 0
         candidate.proportion_of_votes = 0
 
-    # Count votes and calculate proportions
+    # Count votes
     for vote in votes:
         try:
             candidate_id = vote.candidates[0]
@@ -52,12 +61,26 @@ def condorcet_pair(
         candidates[candidate_id].num_votes += 1
 
     if candidate1.num_votes > candidate2.num_votes:
+        explain(
+            f"\t{candidate1.id} ({candidate1.num_votes}, WIN)\tvs\t"
+            f"{candidate2.id} ({candidate2.num_votes})"
+        )
         candidate1.condorcet_score += 1
     elif candidate2.num_votes > candidate1.num_votes:
+        explain(
+            f"\t{candidate1.id} ({candidate1.num_votes})\tvs\t"
+            f"{candidate2.id} ({candidate2.num_votes}, WIN)"
+        )
         candidate2.condorcet_score += 1
+    else:
+        explain(
+            f"\t{candidate1.id} ({candidate1.num_votes})\tvs\t"
+            f"{candidate2.id} ({candidate2.num_votes})\tTIED"
+        )
 
 
 def condorcet(candidates: typing.List[Candidate], votes: typing.List[Vote]):
+    explain("Condorcet pairings:")
     for i, candidate1 in enumerate(candidates):
         for j in range(i + 1, len(candidates)):
             candidate2 = candidates[j]
@@ -68,6 +91,14 @@ def condorcet(candidates: typing.List[Candidate], votes: typing.List[Vote]):
 def find_lowest_condorcet(candidates: typing.List[Candidate]):
     return (
         sorted(candidates, key=lambda c: c.condorcet_score)[0] if candidates else None
+    )
+
+
+def find_lowest_proportion(candidates: typing.List[Candidate]):
+    return (
+        sorted(candidates, key=lambda c: c.proportion_of_votes)[0]
+        if candidates
+        else None
     )
 
 
@@ -102,13 +133,13 @@ def stv_cle(
         for candidate in candidates.values():
             candidate.proportion_of_votes = candidate.num_votes / len(votes)
 
-        # print(f"                 \n======== ROUND {round_} ========")
-        # print("Candidates:")
-        # for candidate in candidates.values():
-        #     print(f"\t{candidate}\t{candidate.proportion_of_votes}\t{candidate.condorcet_score}")
-        # print("Votes:")
-        # for vote in votes:
-        #     print(f"\t{vote}")
+        explain(f"                 \n======== ROUND {round_} ========")
+        explain("Standings:")
+        explain("\tCandidate\tProportion of votes\tCondorcet score")
+        for candidate in candidates.values():
+            explain(
+                f"\t{candidate}\t{candidate.proportion_of_votes}\t{candidate.condorcet_score}"
+            )
 
         # Find new winners
         new_winners = []
@@ -119,20 +150,38 @@ def stv_cle(
         # Is there a winner?
         if new_winners:
             # Yes
+            explain(
+                f"{len(new_winners)} candidates meet the victory quota of {victory_quota}:"
+            )
+            for winner in new_winners:
+                explain(f"\t{winner.id} ({winner.proportion_of_votes})")
             # Remove excess winners based on condorcet score
             num_tied_for_last = len(winners) + len(new_winners) - num_seats
             if num_tied_for_last > 0:
+                explain(
+                    "There are more winners this round than there are seats left. Excluding those with the lowest "
+                    "condorcet scores:"
+                )
                 for i in range(num_tied_for_last):
-                    new_winners.remove(find_lowest_condorcet(new_winners))
+                    lowest = find_lowest_condorcet(new_winners)
+                    lowest_proportion = find_lowest_proportion(
+                        new_winners
+                    ).proportion_of_votes
+                    assert lowest.proportion_of_votes <= lowest_proportion
+                    explain(f"\t{lowest}")
+                    new_winners.remove(lowest)
+            explain(
+                "The following candidates have been declared winners in this round:"
+            )
             for winner in sorted(
                 new_winners, key=lambda c: c.proportion_of_votes, reverse=True
             ):
                 winners.append(candidates.pop(winner.id))
-            # print("Winners:")
-            # for winner in winners:
-            #     print(f"\t{winner}")
+                explain(f"\t{winner.id}")
+
             if len(winners) >= num_seats:
                 # All seats filled
+                explain(f"All {num_seats} seats have been filled.")
                 break
             # Remove new winners from votes
             for vote in votes:
@@ -142,6 +191,15 @@ def stv_cle(
         else:
             # No, eliminate candidate with lowest condorcet score
             last_candidate = find_lowest_condorcet(candidates.values())
+            explain(
+                f"No candidates meet the victory quota of {victory_quota}. Excluding the "
+                f"candidate with the lowest condorcet score:\n\t{last_candidate.id} "
+                f"({last_candidate.condorcet_score})"
+            )
+            lowest_proportion = find_lowest_proportion(
+                candidates.values()
+            ).proportion_of_votes
+            assert last_candidate.proportion_of_votes <= lowest_proportion
             candidates.pop(last_candidate.id)
             # Remove excluded from votes
             for vote in votes:
@@ -150,6 +208,9 @@ def stv_cle(
 
         # Remove empty votes
         votes = list(filter(lambda v: len(v.candidates) > 0, votes))
+        explain(
+            f"{num_seats - len(winners)} of {num_seats} seats are still to be filled."
+        )
 
     return winners
 
@@ -164,7 +225,13 @@ def main():
     parser.add_argument(
         "input_file", help="path to input file with candidates and votes"
     )
+    parser.add_argument(
+        "--explain", action="store_true", help="explain how the result was arrived at"
+    )
     args = parser.parse_args()
+
+    global EXPLAIN
+    EXPLAIN = args.explain
 
     with open(args.input_file) as input_file:
         input_rows = input_file.readlines()
@@ -178,7 +245,9 @@ def main():
     candidates = {id: Candidate(id) for id in candidates_row}
     votes = [Vote(candidates) for candidates in votes_rows]
 
-    for winner in stv_cle(args.num_seats, candidates, votes):
+    winners = stv_cle(args.num_seats, candidates, votes)
+    explain("                 \n======== WINNERS ========")
+    for winner in winners:
         print(winner)
 
 
